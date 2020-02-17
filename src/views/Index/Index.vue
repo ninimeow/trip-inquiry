879a394d5964a36cd2d0769e0274bddf5076a413
liqian <quanshang123@qq.com>
2020-02-17 15:35:35 +0800

    首页

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="views">
+    <img class="banner" src="/static/images/banner.png" alt="banner"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+.banner {
+  width: 100%;
+}
+</style>
